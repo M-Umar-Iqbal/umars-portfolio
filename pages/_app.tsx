3989fedc361ec3b48681764b254707a50414e750
Umar Iqbal <umar.iqbal@staples.com>
2024-05-24 14:30:43 +0500

    Wrap app with react-context.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import Layout from '../components/layout/Layout'
+import { AppContextProvider } from '../context/AppContext'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <>
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <AppContextProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </AppContextProvider>
   </>
 }
 
