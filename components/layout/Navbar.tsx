c27211954a1ffd9e6e68a3ec1aa5fc74e9479c49
Umar Iqbal <umar.iqbal@staples.com>
2024-05-27 14:43:42 +0500

    Fix Navbar deprecated components.

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Disclosure } from '@headlessui/react';
+import { Disclosure, DisclosureButton } from '@headlessui/react';
 import { FaHome, FaUser, FaProjectDiagram, FaEnvelope, FaBlog, FaBars, FaTimes } from 'react-icons/fa';
 import Link from 'next/link';
 
@@ -23,14 +23,14 @@ const Navbar = () => {
           <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
             <div className="relative flex items-center justify-between h-16">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                <DisclosureButton className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
                     <FaTimes className="block h-6 w-6" aria-hidden="true" />
                   ) : (
                     <FaBars className="block h-6 w-6" aria-hidden="true" />
                   )}
-                </Disclosure.Button>
+                </DisclosureButton>
               </div>
               <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="hidden sm:block sm:ml-6">
@@ -58,7 +58,7 @@ const Navbar = () => {
           <Disclosure.Panel className="sm:hidden">
             <div className="px-2 pt-2 pb-3 space-y-1">
               {navigation.map((item) => (
-                <Disclosure.Button
+                <DisclosureButton
                   key={item.name}
                   as={Link}
                   href={item.href}
@@ -70,7 +70,7 @@ const Navbar = () => {
                   )}
                 >
                   {item.name}
-                </Disclosure.Button>
+                </DisclosureButton>
               ))}
             </div>
           </Disclosure.Panel>
