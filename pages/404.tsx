5aa5dc2d744476aab08f4bc828e50e00b20b7431
Umar Iqbal <umar.iqbal@staples.com>
2024-05-27 14:41:42 +0500

    Add 404 page ui.

---
+++ pages/404.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Custom404 = () => {
+    return (
+        <div className="bg-primary text-primary min-h-screen flex flex-col items-center justify-center">
+            <h1 className="text-6xl font-bold mb-4">404</h1>
+            <p className="text-2xl mb-8">Page Not Found</p>
+            <Link href="/" passHref legacyBehavior>
+                <a className="text-blue-500 hover:underline text-lg">
+                    Go back to Home
+                </a>
+            </Link>
+        </div>
+    );
+};
+
+export default Custom404;
