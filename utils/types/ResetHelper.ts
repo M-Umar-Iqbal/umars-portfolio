2fc9eca731fe9c21253ebd21936d15493e6d1272
Umar Iqbal <umar.iqbal@staples.com>
2024-06-13 12:53:21 +0500

    Add rest-helper types file.

---
+++ utils/types/ResetHelper.ts
@@ -0,0 +1,55 @@
+export type ICallType = "getAbout" | "getBlogs" | "getProjects";
+export interface ApiResponse<T> {
+    statusCode: number;
+    data: T;
+};
+export interface IAboutData {
+    name: string;
+    title: string;
+    location: string;
+    bio: string;
+    skills: string[];
+    socialLinks: {
+        github: string;
+        linkedin: string;
+        twitter: string;
+    };
+};
+
+
+export interface IAboutResponse {
+    about: IAboutData
+}
+
+export interface Blog {
+    id: number;
+    title: string;
+    author: string;
+    date: string;
+    content: string;
+    tags: string[];
+};
+
+export interface IBlogsData {
+    blogs: Blog[];
+};
+
+export interface TeamMember {
+    id: number;
+    name: string;
+    role: string;
+};
+
+export interface Project {
+    id: number;
+    name: string;
+    description: string;
+    status: string;
+    startDate: string;
+    endDate: string;
+    team: TeamMember[];
+};
+
+export interface IProjectsData {
+    projects: Project[];
+};
