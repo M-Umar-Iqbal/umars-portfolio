dfc10ea14f0f6e07a654b50b0b30f5fd0685d264
Umar Iqbal <umar.iqbal@staples.com>
2024-08-14 20:36:51 +0500

    Update nav icons.

--- src/components/layout/Navbar/NavLinks.tsx
+++ src/components/layout/Navbar/NavLinks.tsx
@@ -1,46 +1,74 @@
 import React, { FC } from "react";
 import Link from "next/link";
 import { cn } from "@/utils/client-utils";
-import Uses from "@/assets/icons/UsesIcon";
-import ArticlesIcon from "@/assets/icons/ArticlesIcon";
-import ContactIcon from "@/assets/icons/ContactIcon";
-import ProjectsIcon from "@/assets/icons/ProjectsIcon";
+import { BiSolidFolderMinus, BiSolidFile, BiSolidCollection } from "react-icons/bi";
+import { IoIosMailOpen } from "react-icons/io";
+import { usePathname } from "next/navigation";
 
 export const getNavigationLinks = (): {
   label: string | React.JSX.Element;
   href: string;
-  icon: () => React.JSX.Element;
+  icon: (isActiveLink: boolean) => React.JSX.Element;
 }[] => {
   return [
     {
       label: "Projects",
-      icon: () => <ProjectsIcon />,
+      icon: (isActiveLink: boolean) => (
+        <BiSolidFolderMinus
+          className={cn(
+            `duration-150 group-hover:text-purple-500 w-5 h-5`,
+            isActiveLink ? "text-purple-500" : ""
+          )} size={20}
+        />
+      ),
       href: "/projects",
     },
     {
       label: "Articles",
-      icon: () => <ArticlesIcon />,
+      icon: (isActiveLink: boolean) => (
+        <BiSolidFile
+          className={cn(
+            `duration-150 group-hover:text-purple-500 w-5 h-5`,
+            isActiveLink ? "text-purple-500" : ""
+          )} size={20}
+        />
+      ),
       href: "/articles",
     },
     {
       label: "Uses",
-      icon: () => <Uses />,
+      icon: (isActiveLink: boolean) => (
+        <BiSolidCollection
+          className={cn(
+            `duration-150 group-hover:text-purple-500 w-5 h-5`,
+            isActiveLink ? "text-purple-500" : ""
+          )} size={20}
+        />
+      ),
       href: "/uses",
     },
     {
       label: "Contact",
-      icon: () => <ContactIcon />,
+      icon: (isActiveLink: boolean) => (
+        <IoIosMailOpen
+          className={cn(
+            `duration-150 group-hover:text-purple-500 w-5 h-5`,
+            isActiveLink ? "text-purple-500" : ""
+          )} size={20}
+        />
+      ),
       href: "/contact",
     },
   ];
 };
 
-interface LinksProps {
+interface INavLinksProps {
   className?: string;
   closeMenu?: Function;
 }
 
-const Links: FC<LinksProps> = ({ className, closeMenu }) => {
+const NavLinks: FC<INavLinksProps> = ({ className, closeMenu }) => {
+  const pathname = usePathname();
   const closeHandler = () => {
     if (closeMenu) {
       closeMenu();
@@ -50,19 +78,20 @@ const Links: FC<LinksProps> = ({ className, closeMenu }) => {
   const navLinks = getNavigationLinks();
   return (
     <ul className={cn("flex flex-col md:flex-row gap-2", className)}>
-      {navLinks.map(({ href, label, icon }) => (
-        <li key={href} onClick={closeHandler}>
+      {navLinks?.map(({ href, label, icon }) => {
+        const isActiveLink = new RegExp(`^${href}(\\/.*)?$`).test(pathname);
+        return <li key={href} onClick={closeHandler}>
           <Link
             className="text-xl md:text-sm group flex items-center gap-2 py-1 px-1.5 lg:px-2.5"
             href={href}
           >
-            {icon()}
+            {icon(isActiveLink)}
             {label}
           </Link>
         </li>
-      ))}
+      })}
     </ul>
   );
 };
 
-export default Links;
+export default NavLinks;
