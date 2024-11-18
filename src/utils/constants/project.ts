0760513cd4d6cb575881709eb70c7775f1140aca
Umar Iqbal <umar.iqbal@staples.com>
2024-11-18 14:21:05 +0500

    Add more project.

--- src/utils/constants/project.ts
+++ src/utils/constants/project.ts
@@ -1,32 +1,49 @@
 export const projects = [
-    {
-      id: "project-1",
-      name: "Animated Tabs",
-      description:
-        "An animated tab component built with Framer Motion's layoutId prop.",
-      demo: "",
-      github: "",
-      img: "/images/projects/project-1.jpg",
-      projectTags: ["framermotion", "tailwindcss"],
-    },
-    {
-      id: "project-2",
-      name: "Spelling Bee",
-      description:
-        "Spelling Bee 🐝 is a word game that challenges players to construct as many words as they can using pre-selected letters.",
-      demo: "",
-      github: "",
-      img: "/images/projects/project-2.jpg",
-      projectTags: ["reactjs","nextjs", "tailwindcss"],
-    },
-    {
-      id: "project-3",
-      name: "Startup landing page",
-      description: "A startup company landing page designed by using HTML, CSS and bootstrap.",
-      demo: "https://startup-landing-page-steel-zeta.vercel.app/",
-      github: "https://github.com/M-Umar-Iqbal/startup-landing-page",
-      img: "/images/projects/startup-landing-page/img-1.png",
-      projectTags: ["html5","css3", "bootstrap"],
-    },
-  ];
-  
+  {
+    id: "project-1",
+    name: "Startup landing page",
+    description: "A startup company landing page designed by using HTML, CSS and bootstrap.",
+    demo: "https://startup-landing-page-steel-zeta.vercel.app/",
+    github: "https://github.com/M-Umar-Iqbal/startup-landing-page",
+    img: "/images/projects/startup-landing-page/img-1.png",
+    projectTags: ["html5", "css3", "bootstrap"],
+  },
+  {
+    id: "project-2",
+    name: "Social Gathering App",
+    description: "Designed to bridge the gap in how people connect, plan, and organize events.",
+    demo: "",
+    github: "",
+    img: "/images/projects/project-2.png",
+    projectTags: ["flutter", "androidstudio", "xcode"],
+  },
+  {
+    id: "project-3",
+    name: "Handy Restaurant",
+    description: "Designed to reduce the restaurants customer work load and order management.",
+    demo: "",
+    github: "",
+    img: "/images/projects/project-3.png",
+    projectTags: ["java", "androidstudio"],
+  },
+  {
+    id: "project-4",
+    name: "Animated Tabs",
+    description:
+      "An animated tab component built with Framer Motion's layoutId prop.",
+    demo: "",
+    github: "",
+    img: "/images/projects/project-4.jpg",
+    projectTags: ["framermotion", "tailwindcss"],
+  },
+  {
+    id: "project-5",
+    name: "Spelling Bee",
+    description:
+      "Spelling Bee 🐝 is a word game that challenges players to construct as many words as they can using pre-selected letters.",
+    demo: "",
+    github: "",
+    img: "/images/projects/project-5.jpg",
+    projectTags: ["reactjs", "nextjs", "tailwindcss"],
+  },
+];
